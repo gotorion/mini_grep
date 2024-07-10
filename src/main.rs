84f779f5a5d74cff4509b24eca20271ce8b00ae1
Junhui Li <junhui.lee@icloud.com>
2024-07-10 18:57:31 +0800

    add read file support

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 3 {
@@ -7,6 +8,9 @@ fn main() {
     let query = &args[1];
     let file_path = &args[2];
 
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
     println!("Searching for {query}");
     println!("In file {file_path}");
+    println!("With text:\n{contents}")
 }

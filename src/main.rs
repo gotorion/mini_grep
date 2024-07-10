b307bcb7ca3f48b930f77ec49aa20a7f304dbbd1
Junhui Li <junhui.lee@icloud.com>
2024-07-10 19:46:11 +0800

    refactor main using crate

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
+use minigrep::Config;
 use std::env;
-use std::fs;
+use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 3 {
-        panic!("Invalid args");
+    // let config = parse_config(&args);
+    // let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
     }
-    let query = &args[1];
-    let file_path = &args[2];
-
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
-
-    println!("Searching for {query}");
-    println!("In file {file_path}");
-    println!("With text:\n{contents}")
 }
